95419ac7497cd5c46f050e74f571fc7fc5167426
ioisup <ioisup@github.com>
2014-11-21 22:28:43 +0900

    add files

---
+++ test/bootstrap.Spec.js
@@ -0,0 +1,7 @@
+var expect = chai.expect;
+describe("sails", function() {
+   beforeEach(function() { });
+   afterEach(function() { });
+   it('should not fail', function() { expect(true).to.be.true; });
+});
+
---
+++ test/client/client.Spec.js
@@ -0,0 +1,7 @@
+var expect = chai.expect;
+describe("client", function() {
+   beforeEach(function() { });
+   afterEach(function() { });
+   it('should not fail', function() { expect(true).to.be.true; });
+});
+
